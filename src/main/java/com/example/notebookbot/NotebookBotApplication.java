1bef93ae1ba3779673ca1931c2c1515074ba35eb
UkarainskiyS <https://github.com/UkrainskiyS>
2022-01-10 01:18:51 +0300

    check idea git

--- src/main/java/com/example/notebookbot/NotebookBotApplication.java
+++ src/main/java/com/example/notebookbot/NotebookBotApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotebookBotApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NotebookBotApplication.class, args);
     }
